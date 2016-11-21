bd1f852ac71490da20d08352630b6b5266c9c63b
NORTHRIVER\Metad <metad.r2@gmail.com>
2016-11-22 09:29:48 +1100

    Original

---
+++ primeChecker.py
@@ -0,0 +1,8 @@
+n = int(input("What number do you want to check to be a prime number? \n"))
+
+for i in range(2, n):
+    if (n % i) == 0:
+        print(n, "is not a prime")
+        break
+else:
+    print(n, "is a prime")
